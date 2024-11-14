4c9593feea0c03a42734944f3afa2e565c5c97f0
Shoghy Martinez <54950187+Shoghy@users.noreply.github.com>
2024-11-14 16:10:07 -0400

    ✨Creating `index.ts`

---
+++ index.ts
@@ -0,0 +1,23 @@
+export { SetUpWhatsAppAPI, WhatsApp } from "./src/index";
+export { WSRequestError, WSResponseErrorCode } from "./src/error";
+export {
+  BodyComponent,
+  ButtonComponentType,
+  CatalogButtonComponent,
+  ComponentType,
+  HeaderComponent,
+  QuickReplyButtonComponent,
+  UrlButtonComponent,
+} from "./src/templateMessages/components";
+export {
+  ButtonParamType,
+  ButtonPayloadParam,
+  ButtonTextParam,
+  CurrencyParam,
+  DateTimeParam,
+  DocumentParam,
+  ImageParam,
+  TextParam,
+  VideoParam,
+  ParamType,
+} from "./src/templateMessages/params";
