0517371bb0b99e86f3e9b83b8d20e9a72a411ba9
Shoghy Martinez <54950187+Shoghy@users.noreply.github.com>
2025-10-13 11:45:40 -0400

    ♻️Use export * syntax to simplify exports

--- index.ts
+++ index.ts
@@ -1,40 +1,5 @@
-export { SetUpWhatsAppAPI } from "./src/index";
-export type {
-  ContactInfo,
-  MessageInfo,
-  WSErrorJSON,
-  WSErrorResponse,
-  TextMessageResponse,
-  SendTemplateMessageProps,
-  SendTextMessageProps,
-  SetUpProps,
-  TemplateMessageBody,
-  TextMessageBody,
-} from "./src/types";
-export { WSRequestError, WSResponseErrorCode, WSError } from "./src/error";
-export {
-  BodyComponent,
-  ButtonComponentType,
-  CatalogButtonComponent,
-  ComponentType,
-  HeaderComponent,
-  QuickReplyButtonComponent,
-  UrlButtonComponent,
-  BaseButtonComponent,
-} from "./src/templateMessages/components";
-export {
-  ButtonParamType,
-  ButtonPayloadParam,
-  ButtonTextParam,
-  CurrencyParam,
-  DateTimeParam,
-  DocumentParam,
-  ImageParam,
-  TextParam,
-  VideoParam,
-  ParamType,
-  BaseButtonParam,
-  BaseParam,
-  type CurrencyParamProps,
-  type DocumentParamProps,
-} from "./src/templateMessages/params";
+export * from "./src/templateMessages/components";
+export * from "./src/templateMessages/params";
+export * from "./src/error";
+export * from "./src/index";
+export * from "./src/types";
